d8e79c1acfd5cc9b8cda5ae86f6dc4969bba571c
Anwar4123 <anwarhossenmehidi@gmail.com>
2020-10-05 09:31:54 -0700

    done

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://anwarhossen:anwarhossen2391@cluster0.ckhne.mongodb.net/volunteerNetwork?retryWrites=true&w=majority";
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+client.connect(err => {
+  const collection = client.db("volunteerNetwork").collection("register");
+  console.log('database connected');
+
+  app.post("/registered", (req, res) => {
+    const Event = req.body;
+    console.log(Event)
+    collection.insertOne(Event)
+      .then(result => {
+        res.send(result.insertedCount);
+               
+      })
+  })
+  app.get('/lodeUser', (req, res) => {
+    collection.find({})
+    .toArray((err, documents) => {
+      res.send(documents); 
+    })
+  })
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log('hello mama')
+
+})
+
+
